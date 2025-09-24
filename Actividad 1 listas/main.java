3c53a45569fda89ca3d912065560a00584f0ea45
Jhonier Javith Hernández <hernandezjhonier12@gmail.com>
2025-09-24 17:23:27 -0500

    Agrego Actividad 1 (Listas) y Actividad 2 (Mapas)

---
+++ Actividad 1 listas/main.java
@@ -0,0 +1,21 @@
+
+import  Formularios.FrmUsuarios;
+
+
+/**
+ *
+ * @author JHONIER
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       FrmUsuarios j = new FrmUsuarios();
+j.setVisible(true);
+
+        // TODO code application logic here
+    }
+    
+}
---
+++ Actividad 2 mapas/main.java
@@ -0,0 +1,26 @@
+
+import Formularios.FrmEstudiantes;
+import java.util.Map;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author ESTUDIANTES
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        FrmEstudiantes u = new FrmEstudiantes();
+        u.setVisible(true);
+        
+    }
+    
+}
