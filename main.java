aeb4a899159731aee0fcf7beac56f8c03471800d
Jhonier Javith Hernández <hernandezjhonier12@gmail.com>
2025-10-06 11:12:22 -0500

    Actividad Matrices

---
+++ main.java
@@ -0,0 +1,25 @@
+
+import Formularios.Frmmatriz;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author ESTUDIANTES
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+         Frmmatriz u = new Frmmatriz();
+        u.setVisible(true);
+        // TODO code application logic here
+    }
+    
+}
